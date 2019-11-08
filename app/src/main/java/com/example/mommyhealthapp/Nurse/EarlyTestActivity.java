703640c227c6c37a074d8dd60cc542883115417a
Jiaenong <ongje-pa15@student.tarc.edu.my>
2019-11-09 01:43:13 +0800

    modify front end

--- app/src/main/java/com/example/mommyhealthapp/Nurse/EarlyTestActivity.java
+++ app/src/main/java/com/example/mommyhealthapp/Nurse/EarlyTestActivity.java
@@ -1,17 +1,30 @@
 package com.example.mommyhealthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class EarlyTestActivity extends AppCompatActivity {
 
+    private CardView cardViewSectionA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_early_test);
+
+        cardViewSectionA = (CardView)findViewById(R.id.cardViewSectionA);
+        cardViewSectionA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EarlyTestActivity.this, SectionAActivity.class);
+                startActivity(intent);
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
