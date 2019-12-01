8657592692a879fe325f865cf3b4404187d0d01a
Jiaenong <ongje-pa15@student.tarc.edu.my>
2019-12-01 16:29:07 +0800

    modify front end

---
+++ app/src/main/java/com/example/mommyhealthapp/Nurse/MGTTActivity.java
@@ -0,0 +1,16 @@
+package com.example.mommyhealthapp.Nurse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mommyhealthapp.R;
+
+public class MGTTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mgtt);
+    }
+}
