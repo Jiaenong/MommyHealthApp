f1f137e96bef695d29851a9bccdea47e0d0b3dc2
Jiaenong <ongje-pa15@student.tarc.edu.my>
2020-01-16 23:39:33 +0800

    change minor UI

--- app/src/main/java/com/example/mommyhealthapp/Nurse/ui/SearchMother/SearchMotherFragment.java
+++ app/src/main/java/com/example/mommyhealthapp/Nurse/ui/SearchMother/SearchMotherFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -19,6 +20,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
