ae5975b4ebc80935d27d52419150b689f86fee29
mimitao <wernqi@gmail.com>
2020-02-17 22:17:11 +0800

    ui design for pregnancy record

--- app/src/main/java/com/example/mommyhealthapp/Mommy/ui/PregnancyRecord/PregnancyRecordFragment.java
+++ app/src/main/java/com/example/mommyhealthapp/Mommy/ui/PregnancyRecord/PregnancyRecordFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -17,19 +18,20 @@ import com.example.mommyhealthapp.R;
 public class PregnancyRecordFragment extends Fragment {
 
     private PregnancyRecordViewModel dashboardViewModel;
+    private CardView earlyTest, mgtt;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(PregnancyRecordViewModel.class);
+
+        //dashboardViewModel =
+        //        ViewModelProviders.of(this).get(PregnancyRecordViewModel.class);
         View root = inflater.inflate(R.layout.fragment_pregnancy_record, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        //dashboardViewModel.getText().observe(this, new Observer<String>() {
+          //  @Override
+         //   public void onChanged(@Nullable String s) {
+         //       textView.setText(s);
+         //   }
+       // });
         return root;
     }
 }
